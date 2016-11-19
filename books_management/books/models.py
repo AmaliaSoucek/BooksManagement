c31f693dfeb3f36264c922b526972a9bba417b40
Franjo <amalia@ubuntu.(none)>
2016-11-19 14:27:50 +0100

    Setting up project, adding Book, Author and BorrowingRequest models in books, migration 0001 in books

---
+++ books_management/books/models.py
@@ -0,0 +1,30 @@
+from __future__ import unicode_literals
+
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Author(models.Model):
+	first_name = models.CharField(max_length=30)
+	last_name = models.CharField(max_length=30)
+
+
+class Book(models.Model):
+	author = models.ForeignKey(Author)
+	owner = models.ForeignKey(User, on_delete=models.CASCADE)
+	title = models.CharField(max_length=100)		
+
+
+class BorrowingRequest(models.Model):
+
+	PENDING = 0
+	APPROVED = 1
+	DECLINED = 2
+
+	CHOICES = ((PENDING, u'pending'),
+				(APPROVED, u'approved'),
+				(DECLINED, u'declined'))
+    
+	book = models.ForeignKey(Book)
+	borrower = models.ForeignKey(User)
+	status = models.PositiveSmallIntegerField()
