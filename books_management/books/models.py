30f42eac88564423f12144aa61ff155a0c6206d1
Franjo <amalia@ubuntu.(none)>
2016-11-20 15:19:21 +0100

    Adding urls for books and authors

--- books_management/books/models.py
+++ books_management/books/models.py
@@ -6,6 +6,9 @@ class Author(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
 
+    def __str__(self):
+        return "%s %s" % (self.first_name, self.last_name)
+
 
 class Book(models.Model):
     author = models.ForeignKey(Author)
--- books_management/books/urls.py
+++ books_management/books/urls.py
@@ -7,7 +7,7 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^$', views.index),
+    url(r'^/?$', views.BookListView.as_view()),
 
     url(r'register/?$', views.UserRegisterLogin.as_view()),
     url(r'^login/?$', auth.views.login),
@@ -17,5 +17,15 @@ urlpatterns = [
 
     url(r'^authors/?$', views.AuthorListView.as_view()),
     url(r'^authors/create/?$', views.AuthorCreateView.as_view()),
-    url(r'^authors/(?P<pk>[0-9]+)/?$', views.AuthorUpdateView.as_view())
+    url(r'^authors/(?P<pk>[0-9]+)/?$', views.AuthorUpdateView.as_view()),
+
+    url(r'^books/(?P<pk>[0-9]+)/?$', views.BookDetailOrUpdateView.as_view()),
+    url(r'^books/detail/(?P<pk>[0-9]+)/?$',
+        views.BookDetailView.as_view(template_name='books/book_detail.html'),
+        name='book-detail'),
+    url(r'^books/update/(?P<pk>[0-9]+)/?$',
+        views.BookUpdateView.as_view(), name='book-update'),
+    url(r'^books/create/?$', views.BookCreateView.as_view()),
+    url(r'^books/remove/(?P<pk>[0-9]+)/?$', views.BookDeleteView.as_view()),
+    url(r'^books/owned?$', views.OwnedBookListView.as_view()),
 ]
